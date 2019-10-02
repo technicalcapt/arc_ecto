9c189d6dfd9111df2a4ee392d0659beb02661837
technicalcapt <technicalcapt@gmail.com>
2019-10-02 23:51:42 +0700

    add ecto 3.0 - fix compile warnings

--- lib/arc_ecto/definition.ex
+++ lib/arc_ecto/definition.ex
@@ -5,10 +5,12 @@ defmodule Arc.Ecto.Definition do
     quote do
       defmodule Module.concat(unquote(definition), "Type") do
         @behaviour Ecto.Type
-        def type, do: Arc.Ecto.Type.type
+        def type, do: Arc.Ecto.Type.type()
         def cast(value), do: Arc.Ecto.Type.cast(unquote(definition), value)
         def load(value), do: Arc.Ecto.Type.load(unquote(definition), value)
         def dump(value), do: Arc.Ecto.Type.dump(unquote(definition), value)
+        def embed_as(_), do: :self
+        def equal?(term1, term2), do: term1 == term2
       end
 
       def url({%{file_name: file_name, updated_at: updated_at}, scope}, version, options) do
@@ -31,13 +33,15 @@ defmodule Arc.Ecto.Definition do
       end
 
       def url(f, v, options), do: super(f, v, options)
-      
-      def delete({%{file_name: file_name, updated_at: _updated_at}, scope}), do: super({file_name, scope})
+
+      def delete({%{file_name: file_name, updated_at: _updated_at}, scope}),
+        do: super({file_name, scope})
 
       def delete(args), do: super(args)
 
       defp version_url(updated_at, url) do
         stamp = :calendar.datetime_to_gregorian_seconds(NaiveDateTime.to_erl(updated_at))
+
         case URI.parse(url).query do
           nil -> url <> "?v=#{stamp}"
           _ -> url <> "&v=#{stamp}"
